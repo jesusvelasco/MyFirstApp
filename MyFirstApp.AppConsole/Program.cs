54296957ecec71890db13c0010f255f31cc1b6fa
Jesús Velasco <jesus@velasco.biz>
2019-03-16 13:55:45 +0100

    Primer commit

--- MyFirstApp.AppConsole/Program.cs
+++ MyFirstApp.AppConsole/Program.cs
@@ -7,6 +7,8 @@ namespace MyFirstApp.AppConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
         }
     }
 }
