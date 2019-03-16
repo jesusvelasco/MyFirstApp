5f21f7d764d692c57e5cc8d1bbae4f9fa52af195
Jesús Velasco <jesus@velasco.biz>
2019-03-16 13:44:48 +0100

    Add project files.

---
+++ MyFirstApp.AppConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstApp.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
